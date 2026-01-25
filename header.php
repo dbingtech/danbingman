fde5d5feecd641db6ed6fdfa83c71ede3b8000a9
Dan Bingman <techfromtheteacher@gmail.com>
2026-01-25 10:14:28 -0500

    img css

--- header.php
+++ header.php
@@ -10,8 +10,8 @@
 <body>
     <header>
         <div class="heading_images">
-            <img src="images/01_FunShine 1.png" alt="Funshine tours logo with palm trees, sun, and tour bus">
-            <img src="images/script_title.png" alt="Funshine tours written in script font">
+            <img id="image_logo" src="images/01_FunShine 1.png" alt="Funshine tours logo with palm trees, sun, and tour bus">
+            <img id="image_script" src="images/script_title.png" alt="Funshine tours written in script font">
         </div>
         <nav>
             <a href="index.php">Home</a>
