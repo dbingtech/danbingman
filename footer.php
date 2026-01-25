a87cb43de8dd92a2bc391c053002d6de858dafa9
Dan Bingman <techfromtheteacher@gmail.com>
2026-01-25 09:28:15 -0500

    added footer content

--- footer.php
+++ footer.php
@@ -10,6 +10,10 @@
 <body>
     <footer>
             <img src="images/01_FunShine 1.png" alt="Funshine tours logo with palm trees, sun, and tour bus">
+            <div><h1>Contact Us!</h1></div>
+            <div>
+                <p>Email:tours@funshine-tours.com</p>
+                <p>Phone: 216-440-TOUR</p>
     </footer>
     <main>
     <!-- Page-specific content goes here -->
